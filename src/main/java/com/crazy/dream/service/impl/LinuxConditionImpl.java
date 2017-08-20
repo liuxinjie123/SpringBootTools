719387c291857f4ce0b0ec605712dce1b85026f0
liuxinjie123 <jackcodejs@gmail.com>
2017-08-20 11:58:24 +0800

    condition

---
+++ src/main/java/com/crazy/dream/service/impl/LinuxConditionImpl.java
@@ -0,0 +1,12 @@
+package com.crazy.dream.service.impl;
+
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+public class LinuxConditionImpl implements Condition {
+    @Override
+    public boolean matches(ConditionContext conditionContext, AnnotatedTypeMetadata annotatedTypeMetadata) {
+        return conditionContext.getEnvironment().getProperty("os.name").contains("Linux");
+    }
+}
